c9e0108513e592ca259bac819178d916237a974b
LMiguel14 <159493106+LMiguel14@users.noreply.github.com>
2025-02-10 22:40:51 -0500

    Ejercicio 8.2

---
+++ ejercicio8.2/src/Notas/Notas.java
@@ -0,0 +1,58 @@
+package Notas;
+
+public class Notas {
+
+    double[] listaNotas; /* Atributo que identifica un array de notas de tipo double */
+    /**
+    * Constructor de la clase Notas, instancia un array con 5 notas de  tipo double
+    */
+    public Notas() {
+        
+        listaNotas = new double[5]; // Crea un array de 5 notas
+    }
+
+    double calcularPromedio() {
+    double suma = 0;
+
+    for(int i=1; i < listaNotas.length; i++) { // Se recorre el array
+        suma = suma + listaNotas[i]; // Suma las notas del array
+    }
+    /* Obtiene el promedio como la división de la suma de notas sobre el total de notas */
+    return (suma / listaNotas.length);
+    }
+
+    double calcularDesviación() {
+
+        double prom = calcularPromedio(); /* Invoca el método para calcular el promedio */
+        double suma = 0;
+
+        for(int i=0; i < listaNotas.length; i++) {
+        // Aplica fórmula para la sumatoria de elementos
+            suma += Math.pow(listaNotas[i] - prom, 2 );
+        }
+        return Math.sqrt (suma/listaNotas.length ); /* Retorna el cálculo final de la desviación */
+        }
+     
+    double calcularMenor() {
+        double menor = listaNotas[0]; /* Define una variable como la nota menor */
+        for(int i=0; i < listaNotas.length; i++) { // Se recorre el array
+            if (listaNotas[i] < menor) {
+        /* Si un elemento del array es menor que el menor actual, se actualiza su valor */
+                menor = listaNotas[i];
+            }
+        }
+        return menor;
+        }
+       
+    double calcularMayor() {
+        double mayor = listaNotas[0]; /* Define una variable como la
+            nota mayor */
+        for(int i=0; i < listaNotas.length; i++) { // Se recorre el array
+            if (listaNotas[i] > mayor) {
+                /* Si un elemento del array es mayor que el mayor actual, se actualiza su valor */
+                mayor = listaNotas[i];
+            }
+        }
+        return mayor;
+    }
+}
